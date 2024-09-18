4e301a51bb09ef64511b2ab94f417a9f677d4108
Kostenko Oleg <ok609035@gmail.com>
2024-09-19 03:23:16 +0600

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Archive from "./pages/archive/Archive.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
-      <Routes>
-        <Route path="/archive" element={<Archive />} />
-      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
--- src/pages/archive/type.ts
+++
@@ -1,6 +0,0 @@
-export default interface NewspaperItemProps {
-  numbering: number,
-  title: string,
-  data: string,
-  tegs: string
-}
