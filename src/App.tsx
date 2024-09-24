ce9f38270c10fd30b759572ce9ff3751cafdee24
Kostenko Oleg <ok609035@gmail.com>
2024-09-24 21:38:52 +0600

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import RoadstatusAside from "./shared/global_aside/roadStatus_asede/RoadStatusAs
 import FlightsAside from "./shared/global_aside/flights_aside/FlightsAside";
 import TaxiScheduleAside from "./shared/global_aside/taxiSchedule_aside/TaxiScheduleAside";
 import Header from './shared/header/Header';
+import News from "./pages/news/News";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +20,9 @@ function App() {
         {/* <WeatherAside />
         <RoadstatusAside />
         <FlightsAside /> */}
+        <Routes>
+        <Route path="/news" element={<News />} />
+      </Routes>
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import News from "./pages/news/News.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
-      <Routes>
-        <Route path="/news" element={<News />} />
-      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
